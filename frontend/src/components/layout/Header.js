88ba1e046411eb87ab1fcc76f24b116606363eb4
Mr-Nithil <nithilsheshan@gmail.com>
2024-06-28 16:43:31 +0530

    UI Update

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -26,13 +26,14 @@ const Header = () => {
                 <div className="col-12 col-md-3">
                     <div className="navbar-brand">
                         <Link to="/"><img src="/images/fitmanLogo.jpg"/></Link>
+                        <span id="cart" className="ml-3">Cart </span>
                     </div>
                 </div>
 
                 <Search />                
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                     <Link to='/cart' style={{ textDecoration:'none' }}>
-                        <span id="cart" className="ml-3">Cart</span>
+                        <span id="cart" className="ml-3">Cart </span>
                         <span className="ml-1" id="cart_count" style={{ marginLeft: '10px', marginRight: '10px' }}>{cartItems.length}</span>
                     </Link>
 
